d61ebd90a7816d63c636be2e2d0bb2e68eeecae4
Irina Stepanova <stira_04@mail.ru>
2019-10-11 20:43:50 +0600

    feat: add multiply-task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  // перемножаем числа и возвращаем результат в виде строки
+  let result = BigInt(first) * BigInt(second);
+  return result.toString();
 }
